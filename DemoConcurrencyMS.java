985aa834f882bd78fbf510bb4e6ae39629296209
seiwaldm <markus.seiwald2@sbg.ac.at>
2015-11-18 19:28:45 +0100

    Signed-off-by: seiwaldm <markus.seiwald2@sbg.ac.at>

--- DemoConcurrencyMS.java
+++ DemoConcurrencyMS.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.InputStreamReader;
 
 public class DemoConcurrencyMS {
